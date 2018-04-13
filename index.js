ae91c289a5fe34e926b66ca7bf8ed3a249b2a48a
Prabesh Subedi <subediprabesh1@gmail.com>
2018-04-13 03:22:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 'use strict';
+const Dog = function(name, breed, age) {
+  this.name= name;
+  this.breed= breed;
+  this.age = age;
+}
+const Cat = function(name, breed, age) {
+  this.name = name;
+  this.breed = breed;
+  this.age = age;
+}
+const Ferret = function(name, age, foodRequirements) {
+  this.name = name;
+  this.age = age;
+  this.foodRequirements = foodRequirements;
+}
+const Bird = function(name, breed) {
+  this.name = name;
+  this.breed = breed;
+}
+const Fish = function(name, species, waterConditions) {
+  this.name = name;
+  this.waterConditions = waterConditions;
+}
